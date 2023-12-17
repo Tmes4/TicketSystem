51e749d5f747919e3303981bcf7c13d7c13a5a66
Tmes4 <abas_235@hotmail.com>
2023-12-17 20:43:10 +0100

    laste update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Event;
+use App\Models\Role;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,16 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-                // ABBAS
-        \App\Models\User::factory(10)->create();
-        \App\Models\Event::factory(100)->create();
-        \App\Models\Role::factory()->createMany([
+        Role::factory()->createMany([
             ['name' => 'admin'],
             ['name' => 'klant'],
         ]);;
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $adminUser = User::factory()->create([
+            'name' => 'Test-1', 'email' => 'test@test.nl'
+        ]);
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $adminUser->roles()->attach($adminRole);
+        // \App\Models\User::factory()->create(['name' => 'Test-1', 'email' => 'test@test.nl']);
+        \App\Models\User::factory()->create(['name' => 'Test-2', 'email' => 'test-2@test.com']);
+        \App\Models\User::factory(10)->create();
+        \App\Models\Event::factory(100)->create();
     }
 }
